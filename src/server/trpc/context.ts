a06e5760a80d6540b7df8a8c62b915907d1d0911
shrishtiraghav <shrishtiraghav1998@gmail.com>
2024-06-01 21:58:17 +0530

    resolve sqlite access error

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,6 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
-const prisma = new PrismaClient();
+// Workaround to find the db file in production
+const filePath = path.join(process.cwd(), 'prisma/dev.db');
+const config = {
+  datasources: {
+    db: {
+      url: 'file:' + filePath,
+    },
+  },
+};
+console.log("filePath", filePath)
+const prisma = new PrismaClient(config);
 
 export type Context = {
   prisma: PrismaClient;
